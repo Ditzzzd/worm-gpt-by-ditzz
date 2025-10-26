9c17036d92d4e3a4575dcfc001319320ae20880b
Ditzzzd <rkhautal12@gmail.com>
2025-10-26 17:17:12 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+export const BOT_TOKEN = "8479393003:AAHkljBorKniIzR0Ver9o89PZhBqXrGMxpk";
+
+export const WORMGPT_URL = "https://wrmgpt.com/v1/chat/completions";
+
+export const HEADERS = {
+  "x-wormgpt-provider": "worm_gpt",
+  "Content-Type": "application/json",
+};
+
+export const DEFAULT_MAX_TOKENS = 820;
+export const AUTO_REPLY_ENABLED = true;
